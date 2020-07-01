d625538fb659338645420a4cabc201a394fe0d86
Victor <victorapr28@gmail.com>
2020-07-01 08:29:03 -0700

    fix window.location.host

--- src/common/core/utils/get-favicon-from-url.ts
+++ src/common/core/utils/get-favicon-from-url.ts
@@ -6,7 +6,7 @@ export function getFaviconFromUrl(url: string) {
     }
     // relative url to current site
     if ( ! isAbsoluteUrl(url)) {
-        url =  window.location.host;
+        url =  window.location.href;
     }
     const domain = new URL(url).origin;
     return 'https://www.google.com/s2/favicons?domain=' + domain;
